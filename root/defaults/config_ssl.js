4d19ac0b4222d2139a9049ccb131f33357c18025
Bryzgalin <33696103+Bryzgalin@users.noreply.github.com>
2020-11-14 20:38:10 +0200

    Add root dir

---
+++ root/defaults/config_ssl.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+  baseURI: '/',
+  dbCleanInterval: 1000 * 60 * 60,
+  dbPath: '/config/flood-db/',
+  floodServerPort: 3000,
+  maxHistoryStates: 30,
+  pollInterval: 1000 * 5,
+  secret: '<FLOOD_SECRET>',
+  disableUsersAndAuth: true,
+  configUser: {
+    host: 'localhost',
+    port: 5000,
+    socket: true,
+    socketPath: '/run/php/.rtorrent.sock'
+  },
+  ssl: true,
+  sslKey: '/config/nginx/key.pem',
+  sslCert: '/config/nginx/cert.pem',
+};
+
+module.exports = CONFIG;
